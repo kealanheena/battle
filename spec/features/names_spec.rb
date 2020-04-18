638aeea3c72e7f9f2785af773a88af7ed2297358
kealanheena <kealanheena@gmail.com>
2020-04-18 22:44:33 +0100

    Chore: add feature tests for poison attack

--- spec/features/names_spec.rb
+++ spec/features/names_spec.rb
@@ -27,13 +27,37 @@ describe "homepage", type: :feature do
     expect(page).to_not have_content("Kealan's Health: 60")
   end
 
-  scenario "a lose screen should be displayed when a players HP drops to 0" do
+  scenario "poison attacks should decrease health by 5" do
+    sign_in_and_play
+    srand(1)
+    click_button 'Poison'
+    expect(page).to have_content("Harry's Health: 55")
+  end
+
+  scenario "a player should see be able to see if they're poisoned" do
+    sign_in_and_play
+    srand(6)
+    click_button 'Poison'
+    expect(page).to have_content("Harry's (PSN'd)")
+  end
+
+  scenario "a win screen should be displayed when a player 2 HP drops to 0" do
     sign_in_and_play
     Game.instance.attack(59)
     2.times {
       click_button 'Attack'
     }
     expect(page).to have_content("Winner: Kealan")
+  end
+
+  scenario "a lose screen should be displayed when a player 1 HP drops to 0" do
+    sign_in_and_play
+    click_button 'Attack'
+    Game.instance.attack(59)
+    2.times {
+      click_button 'Attack'
+    }
+    expect(page).to have_content("Winner: Harry")
   end
 
 end
