91a26ddc20a2c85d2658288356d4b2b9c5009534
kealanheena <kealanheena@gmail.com>
2020-04-19 15:56:46 +0100

    Chore: make sure that you can add multiple statuses

--- lib/status.rb
+++ lib/status.rb
@@ -4,7 +4,7 @@ class Status
     @current = []
   end
 
-  def add(effect)
-    ['poison']
+  def add(status)
+    @current << status
   end
 end
--- spec/status_spec.rb
+++ spec/status_spec.rb
@@ -8,5 +8,14 @@ describe Status do
     it 'should add a status to the current array' do
       expect(status.add('poison')).to eq ['poison']
     end
+
+    it 'should add different statuses to the current array' do
+      expect(status.add('paralysed')).to eq ['paralysed']
+    end
+
+    it 'should add different statuses to the current array' do
+      status.add('poison')
+      expect(status.add('paralysed')).to eq ['poison', 'paralysed']
+    end
   end
 end
